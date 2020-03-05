9808e2652c0c8397311219650403cc177e80ec2c
uakora <uakora96@gmail.com>
2020-03-05 23:34:19 +0800

    easyui grid 1

---
+++ easyui/easyui.extend.js
@@ -0,0 +1,51 @@
+// datagrid
+$.extend($.fn.datagrid.methods, {
+    addEditor: function (jq, param) {
+        if (param instanceof Array) {
+            $.each(param, function (index, item) {
+                var e = $(jq).datagrid('getColumnOption', item.field);
+                e.editor = item.editor;
+            });
+        } else {
+            var e = $(jq).datagrid('getColumnOption', param.field);
+            e.editor = param.editor;
+        }
+    },
+    removeEditor: function (jq, param) {
+        if (param instanceof Array) {
+            $.each(param, function (index, item) {
+                var e = $(jq).datagrid('getColumnOption', item);
+                e.editor = {};
+            });
+        } else {
+            var e = $(jq).datagrid('getColumnOption', param);
+            e.editor = {};
+        }
+    }
+});
+
+// treegrid
+$.extend($.fn.treegrid.methods, {
+    addEditor: function (jq, param) {
+        if (param instanceof Array) {
+            $.each(param, function (index, item) {
+                var e = $(jq).treegrid('getColumnOption', item.field);
+                e.editor = item.editor;
+            });
+        } else {
+            var e = $(jq).treegrid('getColumnOption', param.field);
+            e.editor = param.editor;
+        }
+    },
+    removeEditor: function (jq, param) {
+        if (param instanceof Array) {
+            $.each(param, function (index, item) {
+                var e = $(jq).treegrid('getColumnOption', item);
+                e.editor = {};
+            });
+        } else {
+            var e = $(jq).treegrid('getColumnOption', param);
+            e.editor = {};
+        }
+    }
+});
