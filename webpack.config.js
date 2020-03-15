786fb96aab99f4283c8887f3b8bde41252844db0
uakora <uakora96@gmail.com>
2020-03-15 23:37:55 +0800

    loader font

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
             {
                 test: /\.(png|jpg|gif|svg)$/,
                 use: ['file-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
             }
         ]
     }
