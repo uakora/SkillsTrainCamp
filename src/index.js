41efaae79c0575752f97f6ade4288fc50da7bb72
uakora <uakora96@gmail.com>
2020-03-16 20:10:54 +0800

    clean asset

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import _ from 'lodash';
-import './style.css';
-import cat from './cat.jpg';
-import XmlData from './xmldata.xml';
 
 
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'Lee'], ' ');
 
-    element.classList.add('hello');
-
-    var catIcon = new Image();
-    catIcon.src = cat;
-    catIcon.height = 150;   
-
-    element.appendChild(catIcon);
-
-    console.log('xml: ', XmlData);
-
+    
     return element;
     
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,30 +6,5 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: ['csv-loader']
-            },
-            {
-                test: /\.xml$/,
-                use: ['xml-loader']
-            }
-        ]
     }
-
 };
